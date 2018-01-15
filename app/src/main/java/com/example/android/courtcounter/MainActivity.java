23c5956c78cbcadbca53fef7d71ca49352bc1c16
Shoxtrem <stanislas97@live.fr>
2018-01-15 13:34:25 +0100

    Added second team B Added interactive Java for Team B Added comments

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +23,56 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-
-    public void displayThreeForTeamA(View view) {
+    /**
+     * Adds 3 point to Team A score.
+     */
+    public void addThreeForTeamA(View view) {
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
     }
-
-    public void displayTwoForTeamA(View view) {
+    /**
+     * Adds 2 point to Team A score.
+     */
+    public void addTwoForTeamA(View view) {
         scoreTeamA += 2;
         displayForTeamA(scoreTeamA);
     }
-
-    public void displayOneForTeamA(View view) {
+    /**
+     * Adds 1 point to Team A score.
+     */
+    public void addOneForTeamA(View view) {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Adds 3 point to Team B score.
+     */
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Adds 2 point to Team B score.
+     */
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Adds 1 point to Team B score.
+     */
+    public void addOneForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
     }
 }
 
